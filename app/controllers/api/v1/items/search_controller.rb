55027d80cf5c9cea9e761bb259a3c0b1fbf828b1
Sam Walker <sgwalker327@gmail.com>
2023-03-29 21:26:41 -0600

    complete items search index action

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,3 +1,16 @@
 class Api::V1::Items::SearchController < ApplicationController
-  
+  def index
+    if params[:min_price] && params[:max_price]
+      @item = Item.price_range_search(params[:min_price], params[:max_price])
+    elsif params[:name]
+      @item = Item.name_search(params[:name])
+    elsif params[:min_price]
+      @item = Item.min_price_search(params[:min_price])
+    elsif params[:max_price]
+      @item = Item.max_price_search(params[:max_price])
+    else
+      render json: { error: 'Invalid Search' }, status: 200
+    end
+    render json: ItemSerializer.new(@item)
+  end
 end
