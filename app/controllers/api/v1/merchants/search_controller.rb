f580b486cdbefe4c7e50dd367dcaf7e4dcb35e37
Sam Walker <sgwalker327@gmail.com>
2023-03-29 21:27:23 -0600

    completed merchants search index action

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def index
     merchant = Merchant.name_search(params[:name])
-    
     if merchant.nil?
-      render json: { error: 'Invalid Search' }, status: 400
+      render json: { error: 'Invalid Search' }, status: 200
     else
       render json: MerchantSerializer.new(merchant)
     end
