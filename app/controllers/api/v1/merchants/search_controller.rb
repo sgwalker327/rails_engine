f1d351573ede49b19c4bf9a609060352e30fd3b0
Sam Walker <sgwalker327@gmail.com>
2023-03-31 10:07:56 -0600

    final commit

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Merchants::SearchController < ApplicationController
   def show
     merchant = Merchant.name_search(params[:name])
     if merchant.nil?
-      render json: { error: 'Invalid Search', data: {} }, status: 200
+      render json: { errors: 'Invalid Search', data: {} }, status: 200
     else
       render json: MerchantSerializer.new(merchant)
     end
